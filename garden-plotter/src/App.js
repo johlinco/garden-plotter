5b577e6c7850d0ae094fcc44c0a5785ce113282a
Lincoln Hoey Moore <johlinco@gmail.com>
2024-03-26 16:13:32 -0400

    Generated PlantSelector Component
    No code added. just added the component file location.

--- garden-plotter/src/App.js
+++ garden-plotter/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import GardenBed from './components/GardenBed.js'
+import PlantSelector from './components/PlantSelector';
 
 function App() {
   return (
     <div className="App">
-      <p>HELLO WORLD</p>
+      <PlantSelector />
       <GardenBed />
     </div>
   );
